2ba80ce078abf677fb31357c8b724478d3d449ee
caykevictor94-creator <caykevictor94@gmail.com>
2026-04-12 00:02:12 -0300

    adiciona comentario antes do metodo main

--- Calculadora.java
+++ Calculadora.java
@@ -15,6 +15,7 @@ public class Calculadora {
     }
 
     // Método principal para testar
+    // Ponto de entrada da aplicacao
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
